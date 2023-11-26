23e12f4dbb2cbc94a0463d175ac29e5ef53f3a82
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-11-26 17:54:22 +0530

    added images to carousel

--- src/customer/components/HomeCarousel/MainCarousel.js
+++ src/customer/components/HomeCarousel/MainCarousel.js
@@ -1,24 +1,17 @@
 import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
+import { HomeCarouselData } from "./HomeCarouselData";
 
-const items = [
-  <div className="item" data-value="1">
-    1
-  </div>,
-  <div className="item" data-value="2">
-    2
-  </div>,
-  <div className="item" data-value="3">
-    3
-  </div>,
-  <div className="item" data-value="4">
-    4
-  </div>,
-  <div className="item" data-value="5">
-    5
-  </div>,
-];
+const MainCarousel = () => {
+  const items = HomeCarouselData.map((item) => <img className="cursor-pointer" role="presentation" src={item.image_url} alt="" />);
 
-const MainCarousel = () => <AliceCarousel mouseTracking items={items} controlsStrategy="alternate" />;
+  return (
+    <div>
+      <AliceCarousel mouseTracking items={items} controlsStrategy="alternate"
+      disableButtonsControls autoPlay autoPlayInterval={2000} infinite />
+    </div>
+  );
+};
+
 export default MainCarousel;
