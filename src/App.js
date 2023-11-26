f146f235e99feca7caa9e114ae3975d865ccabb4
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-11-26 17:00:42 +0530

    added very basic skeleton for homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+import HomePage from './customer/pages/HomePage/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      The Hideaway Shop
+    <div >
+      <div>
+        <Navigation />
+      </div>
+      <div>
+        <HomePage />
+      </div>
     </div>
   );
 }
