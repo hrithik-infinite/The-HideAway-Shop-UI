73a26a33011e523bbae185c704b6ef41a381f753
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-11-30 22:17:50 +0530

    added product page skeleton

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import Navigation from './customer/components/Navigation/Navigation';
-import Product from './customer/components/Product/Product';
-import Footer from './customer/components/footer/footer';
-import HomePage from './customer/pages/HomePage/HomePage';
+import "./App.css";
+import Navigation from "./customer/components/Navigation/Navigation";
+import Product from "./customer/components/Product/Product";
+import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
+import Footer from "./customer/components/footer/footer";
+import HomePage from "./customer/pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div >
-      <div className='z-50'>
+    <div>
+      <div className="z-50">
         <Navigation />
       </div>
       <div>
         {/* <HomePage /> */}
-        <Product />
+        {/* <Product /> */}
+        <ProductDetails />
       </div>
       <Footer />
     </div>
