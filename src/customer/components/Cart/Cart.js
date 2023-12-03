ac3e9b25bda2966ca50ad7b4e6b86e9437dddd0c
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-03 16:27:29 +0530

    added more dtls to cart page

--- src/customer/components/Cart/Cart.js
+++ src/customer/components/Cart/Cart.js
@@ -1,13 +1,50 @@
 import React from "react";
 import CartItem from "./CartItem";
+import { Button } from "@mui/material";
 
 const Cart = () => {
-    return (
-        <div>{
-            [1,1,1,1].map((item) => <CartItem />)}</div>
-    )
+  return (
+    <div>
+      <div className="lg:grid grid-cols-3 lg:px-16 relative">
+        <div className="lg:col-span-2 lg:px-5 bg-white">
+          <div className=" space-y-3">
+            {[1, 1, 1, 1].map((item) => (
+              <CartItem />
+            ))}
+          </div>
+        </div>
+        <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0 ">
+          <div className="border p-5 bg-white shadow-lg rounded-md">
+            <p className="font-bold opacity-60 pb-4">PRICE DETAILS</p>
+            <hr />
 
-
-}
+            <div className="space-y-3 font-semibold">
+              <div className="flex justify-between pt-3 text-black ">
+                <span>Price</span>
+                <span>₹ 99</span>
+              </div>
+              <div className="flex justify-between">
+                <span>Discount</span>
+                <span className="text-green-700">₹ 99</span>
+              </div>
+              <div className="flex justify-between">
+                <span>Delivery Charge</span>
+                <span className="text-green-700">₹ 99</span>
+              </div>
+              <hr />
+              <div className="flex justify-between font-bold text-lg">
+                <span>Total Amount</span>
+                <span className="text-green-700">₹ 99</span>
+              </div>
+            </div>
+            <Button variant="contained" sx={{ padding: ".8rem 2rem", marginTop: "2rem", width: "100%" }}>
+              Check Out
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
