14248db73359ace11bf1404a38c07e83ba3e00cf
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-10 21:36:51 +0530

    login api integration

--- src/customer/Auth/Login.js
+++ src/customer/Auth/Login.js
@@ -1,8 +1,22 @@
 import { Alert, Button, Grid, Snackbar, TextField } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { getUser, login } from "../../Redux/Auth/Action";
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { auth } = useSelector((store) => store);
+  const jwt = localStorage.getItem("jwt");
+  useEffect(() => {
+    if (jwt) {
+      dispatch(getUser(jwt));
+    }
+  }, [jwt]);
+
+  // useEffect(() => {
+  //   if (auth.user || auth.error) setOpenSnackBar(true)
+  // }, [auth.user]);
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -12,6 +26,7 @@ const Login = () => {
       password: data.get("password"),
     };
     console.log("user data", userData);
+    dispatch(login(userData));
   };
   return (
     <React.Fragment className=" shadow-lg ">
